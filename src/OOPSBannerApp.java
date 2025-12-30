d8807ac925bb24bcd8e778a650e9fd6c590fc8da
sharatyenni <srikanthyenni.9@gmail.com>
2025-12-30 15:00:46 +0530

    UC3: Refactor banner logic into static methods

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,15 +1,23 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
+        String[] banner = buildBanner();
+        printBanner(banner);
+    }
 
-        String[] banner = {
+    // Builds the OOPS banner
+    static String[] buildBanner() {
+        return new String[] {
             " ***    ***   *****  **** ",
             "*   *  *   *  *      *   *",
             "*   *  *   *  *****  **** ",
             "*   *  *   *      *  *    ",
             " ***    ***   *****  *    "
         };
+    }
 
+    // Prints the banner
+    static void printBanner(String[] banner) {
         for (String line : banner) {
             System.out.println(line);
         }
